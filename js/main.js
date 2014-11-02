cf904063f6d1dcbc587057a798f0a097a953949d
IPopov <ip.webmaster.spb@gmail.com>
2014-11-02 17:13:58 +0300

    Простой пример работы requireJS. Аттрибут data-main в теги script, задает точку входа.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-var MyMath = (function() {
-
-    //С этим патерном вы можете использовать обычное описание функций:
-
-    function add(a, b) {
-        return a + b;
-    }
-
-    return {
-      add : add
-    };
-})();
-console.log(MyMath.add(1, 2));
+require(['MyMath'], function(MyMath){
+   console.log(MyMath.add(1, 2));
+});
